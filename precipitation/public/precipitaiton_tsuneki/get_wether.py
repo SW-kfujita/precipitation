af658c5bd9bf82f48838491c60eb597252c7b95f
Ryu <ron.ryu0312@outlook.jp>
2019-03-02 17:04:11 +0900

    get_weather&,main corection

--- precipitation/public/precipitaiton_tsuneki/get_wether.py
+++ precipitation/public/precipitaiton_tsuneki/get_wether.py
@@ -11,7 +11,7 @@ def get_tenki_jp(now):
     dfs = pd.read_html(url,encoding='utf-8')    
     df = dfs[0].head()
     data = df.at[0,now]
-    return data
+    return data.strip("%")
 
 def get_weather_news():
     url = 'https://www.google.com/search?q=%E6%B8%AF%E5%8C%BA+%E9%99%8D%E6%B0%B4%E7%A2%BA%E7%8E%87&oq=%E6%B8%AF%E5%8C%BA%E3%80%80%E9%99%8D%E6%B0%B4%E7%A2%BA%E7%8E%87&aqs=chrome..69i57j0l5.9041j1j4&sourceid=chrome&ie=UTF-8'
@@ -39,23 +39,6 @@ def get_jma(now):
         col.append(str(td).replace("<td align=\"right\">","").replace("<td align=\"left\">","").replace("</td>",""))
     if now in col :
                precipitation = col[col.index(now) + 1 ] 
-    return precipitation
+    return precipitation.strip("%")
 
 
-now = datetime.now().strftime("%H")
-now = int(now)
-if 0 <= now < 6:
-    now = '00-06'
-elif 6 <= now < 12:
-    now = '06-12'
-elif 12 <= now < 18:
-    now = '12-18'
-elif 18 <= now < 24:
-    now = '18-24'
-
-
-data = get_tenki_jp(now)
-print(data)
-
-data2 = get_jma(now)
-print(data2)
--- precipitation/public/precipitaiton_tsuneki/main.py
+++ precipitation/public/precipitaiton_tsuneki/main.py
@@ -1,14 +1,34 @@
 from flask import Flask, render_template, request
 import random
+from datetime import datetime
+from get_wether import get_jma,get_tenki_jp
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     start = request.args.get("start", "")
     average = random.randint(0, 100)
+    
+    now = datetime.now().strftime("%H")
+    now = int(now)  
+    if 0 <= now < 6:
+        now = '00-06'
+    elif 6 <= now < 12:
+        now = '06-12'
+    elif 12 <= now < 18:
+        now = '12-18'
+    elif 18 <= now < 24:
+        now = '18-24'
+    
+    data2 =get_tenki_jp(now)
+
+    data1 = get_jma(now)
+    average = (int(data2)+int(data1))/2
     return render_template("index.html",
                             start=start,
-                            average=average)
+                            average=average,
+                            data1 = data1,
+                            data2 = data2)
 
 if __name__ == '__main__':
     app.run( host = '0.0.0.0' )
