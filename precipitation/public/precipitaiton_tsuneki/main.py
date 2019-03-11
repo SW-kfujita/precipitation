a697486a1917a56be8b25a5993dc2cf5f205c685
Ryu <ron.ryu0312@outlook.jp>
2019-03-11 17:11:25 +0900

    commit

--- precipitation/public/precipitaiton_tsuneki/main.py
+++ precipitation/public/precipitaiton_tsuneki/main.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, request
 import random
 from datetime import datetime
-from get_wether import get_jma,get_tenki_jp
+from get_wether import get_jma, get_tenki_jp
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     start = request.args.get("start", "")
     average = random.randint(0, 100)
-    
+
     now = datetime.now().strftime("%H")
-    now = int(now)  
+    now = int(now)
     if 0 <= now < 6:
         now = '00-06'
     elif 6 <= now < 12:
@@ -19,16 +20,19 @@ def index():
         now = '12-18'
     elif 18 <= now < 24:
         now = '18-24'
-    
-    data2 =get_tenki_jp(now)
+
+    data2 = get_tenki_jp(now)
 
     data1 = get_jma(now)
-    average = (int(data2)+int(data1))/2
-    return render_template("index.html",
-                            start=start,
-                            average=average,
-                            data1 = data1,
-                            data2 = data2)
+    data3 = 40
+    average = (int(data2)+int(data1)+data3)/3
+    return render_template("precipitation_vs.html",
+                           start=start,
+                           average=round(average, 1),
+                           data1=data1,
+                           data2=data2,
+                           data3=data3)
+
 
 if __name__ == '__main__':
-    app.run( host = '0.0.0.0' )
+    app.run(host='0.0.0.0')
